ccb2e77309973317ffe108391de040721d85e73a
leandrogomes777 <61293166+leandrogomes777@users.noreply.github.com>
2020-05-19 20:10:35 -0300

    inicial

---
+++ AvisaSolution/AvisaSolution/Data/AvisaSolutionContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvisaSolution.Models
+{
+    public class AvisaSolutionContext : DbContext
+    {
+        public AvisaSolutionContext (DbContextOptions<AvisaSolutionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AvisaSolution.Models.User> User { get; set; }
+    }
+}
---
+++ AvisaSolution/AvisaSolution/Service/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AvisaSolution.Models;
+
+namespace AvisaSolution.Service
+{
+    public class UserService : IUserService
+    {
+        public void AddUser(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
