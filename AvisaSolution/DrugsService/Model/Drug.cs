50b7540f4862d64ff1f530e6fcba681acbb41d12
leandrogomes777 <61293166+leandrogomes777@users.noreply.github.com>
2020-05-21 14:41:40 -0300

    Serviço de remédios

---
+++ AvisaSolution/DrugsService/Model/Drug.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrugsService.Model
+{
+    public class Drug
+    {
+        public long ID
+        {
+            get;
+            set;
+        }
+
+        public string DrugName
+        {
+            get;
+            set;
+        }
+
+        public string Holder
+        {
+            get;
+            set;
+        }
+
+        public string Medicine
+        {
+            get;
+            set;
+        }
+
+        public string Record
+        {
+            get;
+            set;
+        }
+
+        public string Concentration
+        {
+            get;
+            set;
+        }
+
+        public string PharmaceuticalForm
+        {
+            get;
+            set;
+        }
+    }
+}
